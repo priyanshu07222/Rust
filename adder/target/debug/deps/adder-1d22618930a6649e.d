a30a539771e89dcfe7eb0eac99d9749e841a6402
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-09-12 00:07:59 +0530

    tests

---
+++ adder/target/debug/deps/adder-1d22618930a6649e.d
@@ -0,0 +1,7 @@
+/home/priyanshu/web3/Rust/adder/target/debug/deps/libadder-1d22618930a6649e.rmeta: src/lib.rs
+
+/home/priyanshu/web3/Rust/adder/target/debug/deps/libadder-1d22618930a6649e.rlib: src/lib.rs
+
+/home/priyanshu/web3/Rust/adder/target/debug/deps/adder-1d22618930a6649e.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-abe730681f5db86c.d
@@ -0,0 +1,5 @@
+/home/priyanshu/web3/Rust/adder/target/debug/deps/adder-abe730681f5db86c: src/lib.rs
+
+/home/priyanshu/web3/Rust/adder/target/debug/deps/adder-abe730681f5db86c.d: src/lib.rs
+
+src/lib.rs:
